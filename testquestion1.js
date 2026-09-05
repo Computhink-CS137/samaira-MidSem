003c37b3efc72d913746d160a3d0d1341b77c804
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-05 06:44:59 +0000

    Sep 5, 2026, 6:44 AM

--- testquestion1.js
+++ testquestion1.js
@@ -4,9 +4,9 @@ function setup() {
 }
 
 function draw() {
-    cricle(100, 200, 200)
-    rect(410, 100, 180, 180)
-    triangle(210, 300, 390, 300, 300, 100)
+    cricle(100, 200, 200);
+    rect(410, 100, 180, 180);
+    triangle(210, 300, 390, 300, 300, 100);
 }
 
 // for( let i = 0, i < 10, i++) {

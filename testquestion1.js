e0d54e7fca4830a1f4e17928b7b7f78923b8736f
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-05 06:30:22 +0000

    Sep 5, 2026, 6:30 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,11 @@
+function setup() {
+new Canvas(600, 800);
+background(220);
+strokeWieght(10);
+circle(50, 50, 50);   
+}
 
+// for( let i = 0, i < 10, i++) {
+//     console.log(i); 
+//     // print in java
+// }

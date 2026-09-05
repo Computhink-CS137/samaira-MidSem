3fff9207021f5230b30fa21e19f073d60f1c8278
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-05 07:04:08 +0000

    Sep 5, 2026, 7:04 AM

--- testquestion2.js
+++ testquestion2.js
@@ -7,7 +7,7 @@ function draw() {
     for( let i = 0; i < 10; i++) {
         noStroke();
         fill(0 + i * 25)
-        circle(30 + i * 30, 30 + i * 30, 50)
+        circle(30 + i * 60, 30 + i * 60, 50)
     }
 }
 

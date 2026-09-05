e1d2a8b42608c7b07b5d52469fb2e8f67066efb3
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-05 07:03:00 +0000

    Sep 5, 2026, 7:03 AM

--- testquestion1.js
+++ testquestion1.js
@@ -8,12 +8,9 @@ function draw() {
     stroke(255, 0, 0);
     fill(0);
     strokeWeight(10);
-    cricle(100, 200, 200);
+    circle(100, 200, 200);
     rect(410, 100, 180, 180);
     triangle(210, 300, 390, 300, 300, 100);
 }
 
-// for( let i = 0, i < 10, i++) {
-//     console.log(i); 
-//     // print in java
-// }
+
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,13 @@
 
+function setup() {
+    new Canvas(600, 600);
+    background(220);
+}
+function draw() {
+    for( let i = 0; i < 10; i++) {
+        noStroke();
+        fill(0 + i * 25)
+        circle(30 + i * 30, 30 + i * 30, 50)
+    }
+}
+
